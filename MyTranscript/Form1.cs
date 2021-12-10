83f874dcd3dc999e6e2fd631800ff80de62c8076
Pimlapus <63030065@kmitl.ac.th>
2021-12-10 15:09:20 +0700

    Dongburi

--- MyTranscript/Form1.cs
+++ MyTranscript/Form1.cs
@@ -16,7 +16,13 @@ namespace MyTranscript
         {
             InitializeComponent();
         }
-
+        class Course
+        {
+            public string CourseID { get; set; }
+            public string CourseName { get; set; }
+            public string CourseCredit { get; set; }
+            public string Grade { get; set; }
+        }
         private void label5_Click(object sender, EventArgs e)
         {
 
